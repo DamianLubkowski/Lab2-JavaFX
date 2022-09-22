0e78be9cefaeb884d65812eae6ba4769c11db4c2
DamianLubkowski <multiclick@wp.pl>
2022-09-22 08:49:19 +0200

    Punkt 5

--- src/main/java/com/example/lab2javafx/HelloController.java
+++ src/main/java/com/example/lab2javafx/HelloController.java
@@ -4,14 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
 
     public void onBtnAction(ActionEvent actionEvent) {
         System.out.println("Kliknięcie nr " + ++clickCounter);
         btnMain.setText("I jeszcze raz");
+        txtLicznik.setText(Integer.toString(clickCounter));
     }
 }
