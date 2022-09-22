7816df784d99db993e67a12268f47b39aa8f3c4a
DamianLubkowski <multiclick@wp.pl>
2022-09-22 09:06:00 +0200

    Punkt 6

--- src/main/java/com/example/lab2javafx/HelloController.java
+++ src/main/java/com/example/lab2javafx/HelloController.java
@@ -9,12 +9,29 @@ import javafx.scene.control.TextField;
 public class HelloController {
     @FXML
     public Button btnMain;
-    public TextField txtLicznik;
-    int clickCounter = 0;
+    public TextField radius;
+    public TextField height;
+    public TextField weight;
+    public TextField mass_normal;
+    public TextField mass_enhanced;
+    public TextField price_normal;
+    public TextField price_enhanced;
 
     public void onBtnAction(ActionEvent actionEvent) {
-        System.out.println("Kliknięcie nr " + ++clickCounter);
-        btnMain.setText("I jeszcze raz");
-        txtLicznik.setText(Integer.toString(clickCounter));
+        double r = Double.valueOf( radius.getText() );
+        double h = Double.valueOf( height.getText() );
+        double w = Double.valueOf( weight.getText() );
+
+        if (r != 0 && h != 0 && w != 0) {
+            double V = ((Math.PI*r*r)*2 + 2*Math.PI*r*h) * w;
+            mass_normal.setText(Double.toString(V));
+            mass_enhanced.setText(Double.toString(V*1.735));
+
+            double price_n = Math.round(V*5 * 100.0)/100.0;
+            double price_e = Math.round(V*1.735*10 * 100.0)/100.0;
+
+            price_normal.setText(Double.toString(price_n) + " zł");
+            price_enhanced.setText(Double.toString(price_e) + " zł");
+        }
     }
 }
